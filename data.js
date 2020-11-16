d44b1d73388a74ae9b5fb274dc79f5fab3145aa0
root <root@ip-172-31-22-77.us-east-2.compute.internal>
2020-11-16 16:20:27 +0000

    Monday verbiage

--- data.js
+++ data.js
@@ -5,7 +5,7 @@ var data = [
     "questions": [
       {
         "points": 100,
-        "question": "fromdev",
+        "question": "Moooonday",
         "answer": "... to use Jenkins"
       },
       {
